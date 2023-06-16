fda957a3c4a48e9c9181d92ce278881a8f621ee3
Haven <haven.eldebaque@gmail.com>
2023-06-16 14:24:31 +1000

    Add project files.

---
+++ WeekThreeCodeChallengeExTwo/Job.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekThreeCodeChallengeExTwo
+{
+    internal class Job
+    {
+        // Data fields
+        private string jobDescription;
+        private double time;
+        private double hourRate;
+        private double totalFee;
+
+        // Properties
+        public string JobDescription
+        {
+            get { return jobDescription; }
+            set { jobDescription = value; }
+        }
+        
+        public double Time
+        {
+            get { return time; }
+            set { time = value;
+                CalculateTotalFees();
+            }
+        }
+        public double HourRate
+        {
+            get { return hourRate; }
+            set { hourRate = value;
+                CalculateTotalFees();
+            }
+        }
+        public double TotalFee { get; }
+
+        // constructors
+        // The constructor below is to eliminate an error in the Program.cs file 
+        public Job()
+        {
+
+        }
+
+        // writing a second constructor 
+        public Job(string jobDescription, double time, double hourRate)
+        {
+            jobDescription = JobDescription;
+            time = Time;
+            hourRate = HourRate;
+        }
+
+        // method to display job information
+        public void DisplayJobInformation()
+        {
+            Console.WriteLine("Job description is: {0}, takes {1} hours time, with an hour rate of {2} costs: {3}", JobDescription, Time, HourRate, totalFee);
+        }
+
+        // method to change the HourRate if curent rate is < $20
+        public void UpdateJobHourReate(double amount) 
+        {
+            if (hourRate < 20)
+            {
+                hourRate += amount;
+            }
+        }
+
+        // method to calculate total fees
+        public void CalculateTotalFees()
+        {
+            totalFee = Time * HourRate;
+        }
+    }
+}
---
+++ WeekThreeCodeChallengeExTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Exercise 2
+
+// Write a program named DemoJobs for Harold’s Home Services. The program should instantiate several Job objects and demonstrate their
+// methods. The Job class contains four data fields: descriptor (for example, “wash windows”), time in hours to complete (for example, 3.5),
+// the per-hour rate charged (for example, $35.00), and total fee (hourly rate times hours).
+
+// Include properties to get and set each field except the total fee — that field will be read-only and its value is calculated each time
+// the hourly fee or the number of hours is set.
+
+// Write a method for the Job class to display information about a Job object. The Job class also contains a method to change the per-hour
+// rate by adding an amount of money to the current rate if the current rate of the job is less than $20.00.
+
+// Write a Main() method that demonstrates all the methods work correctly.
+
+using WeekThreeCodeChallengeExTwo;
+
+class DemoJobs
+{
+    public static void Main()
+    {
+        Job job1 = new Job();
+        job1.JobDescription = "wash windows";
+        job1.Time = 3.5;
+        job1.HourRate = 5;
+
+     
+        // method 
+        job1.DisplayJobInformation();
+    }
+}
